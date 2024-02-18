2e266a1199655e1861af8a6fb572072e0a3581eb
victoria <victoria.moretti@hotmail.es>
2024-02-18 20:40:17 +0000

    adding apiUrl

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+function displayRecepie(response) {}
+
 function generateRecepie(event) {
 	event.preventDefault();
 
+	let apiKey = "t7208a38bb3e0eaafeo46762934523cb";
+	let prompt = "Give me a simple and quick recepie for cooking";
+	let context = "Make sure to provide a clear, short and precise answer";
+	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+	axios.get(apiUrl).then(displayRecepie);
+
 	new Typewriter("#recepie", {
-		strings: ["The new recepie is loading.."],
+		strings: "The new recepie is loading..",
 		autoStart: true,
-		Delay: 1,
+		delay: 30,
 		cursor: "",
 	});
 }
