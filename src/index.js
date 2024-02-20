8718763124a346dc2c64911cb356bc360d1a5bd6
victoria <victoria.moretti@hotmail.es>
2024-02-20 07:43:11 +0000

    improving user experience

--- src/index.js
+++ src/index.js
@@ -15,8 +15,12 @@ function generateRecepie(event) {
 	let apiKey = "t7208a38bb3e0eaafeo46762934523cb";
 	let prompt = `Give me a simple and quick recepie with less than 20 minutes of preparation cooking using ${userInstructions.value} as the main ingredient of the recepie`;
 	let context =
-		"You are a very good chef and know many recepies. Provide a recepie with no more than 7 ingredientes using basic HTML and in a <li> format less. Make sure to use the user's input and provide a clear, short and precise answer. make sure to have a title ";
+		"You are a very good chef and know many recepies. Provide a recepie with no more than 7 ingredientes using basic HTML and in a <li> format. Make sure to use the user's input and provide a clear, short and precise answer. Make sure to have a title and EXPLAIN the process in a <li> format.";
 	let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+	let recepieElement = document.querySelector("#recepie");
+	recepieElement.classList.remove("hidden");
+	recepieElement.innerHTML = `<div class="generating"> ⌛ Generating a recepie about ${userInstructions.value}..</div>`;
 
 	console.log("generating recepie..");
 	console.log(`${prompt}`);
